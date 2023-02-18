cf73c61565c1939a2c5f8c0e6947c7bc952c8427
Ivan <vanyablya003@gmail.com>
2023-02-18 16:16:15 +0300

    test2

--- src/Home/home.service.ts
+++ src/Home/home.service.ts
@@ -6,8 +6,8 @@ const newTitlesUrl = '?season=2023&status=ongoing&order=ranked&limit=10';
 @Injectable()
 export class HomeService {
   getNewTitles() {
-    const animes = api<[any]>(shikimori_api + 'animes' + newTitlesUrl);
+    // const animes = api<[any]>(shikimori_api + 'animes' + newTitlesUrl);
 
-    return animes;
+    return newTitlesUrl;
   }
 }
