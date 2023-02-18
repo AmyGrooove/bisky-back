a373a15fdfb7efa8ce2dd92e46d72972062cc5c1
Ivan <vanyablya003@gmail.com>
2023-02-18 19:13:33 +0300

    test anime get point

---
+++ constants/types.ts
@@ -0,0 +1,104 @@
+export interface AniemShort {
+  id: number;
+  name: string;
+  russian: string;
+  image: Image;
+  url: string;
+  kind: string;
+  score: string;
+  status: string;
+  episodes: number;
+  episodes_aired: number;
+  aired_on: string;
+  released_on: string;
+}
+
+export interface AnimeFull {
+  id: number;
+  name: string;
+  russian: string;
+  image: Image;
+  url: string;
+  kind: string;
+  score: string;
+  status: string;
+  episodes: number;
+  episodes_aired: number;
+  aired_on: string;
+  released_on: string;
+  rating: string;
+  english: string[];
+  japanese: string[];
+  synonyms: any[];
+  license_name_ru: string;
+  duration: number;
+  description: string;
+  description_html: string;
+  description_source: any;
+  franchise: any;
+  favoured: boolean;
+  anons: boolean;
+  ongoing: boolean;
+  thread_id: number;
+  topic_id: number;
+  myanimelist_id: number;
+  rates_scores_stats: RatesScoresStat[];
+  rates_statuses_stats: RatesStatusesStat[];
+  updated_at: string;
+  next_episode_at: any;
+  fansubbers: string[];
+  fandubbers: string[];
+  licensors: string[];
+  genres: Genre[];
+  studios: Studio[];
+  videos: Video[];
+  screenshots: Screenshot[];
+  user_rate: any;
+}
+
+export interface Image {
+  original: string;
+  preview: string;
+  x96: string;
+  x48: string;
+}
+
+export interface RatesScoresStat {
+  name: number;
+  value: number;
+}
+
+export interface RatesStatusesStat {
+  name: string;
+  value: number;
+}
+
+export interface Genre {
+  id: number;
+  name: string;
+  russian: string;
+  kind: string;
+}
+
+export interface Studio {
+  id: number;
+  name: string;
+  filtered_name: string;
+  real: boolean;
+  image: string;
+}
+
+export interface Video {
+  id: number;
+  url: string;
+  image_url: string;
+  player_url: string;
+  name: string;
+  kind: string;
+  hosting: string;
+}
+
+export interface Screenshot {
+  original: string;
+  preview: string;
+}
--- src/Home/home.service.ts
+++ src/Home/home.service.ts
@@ -1,36 +1,51 @@
 import { Injectable } from '@nestjs/common';
 import { http, shikimori_api } from '../../constants';
-import { firstValueFrom } from 'rxjs';
+import { AniemShort, AnimeFull, Screenshot } from '../../constants/types';
 
 const newTitlesUrl = '?season=2023&status=ongoing&order=ranked&limit=10';
 
-export interface Anime {
+export interface HomeAnime {
   id: number;
   name: string;
-  russian: string;
-  image: Image;
-  url: string;
-  kind: string;
+  image: string;
   score: string;
-  status: string;
-  episodes: number;
-  episodes_aired: number;
-  aired_on: string;
-  released_on: any;
-}
-
-interface Image {
-  original: string;
-  preview: string;
-  x96: string;
-  x48: string;
+  screenshots: string[];
+  genres: string[];
 }
 
 @Injectable()
 export class HomeService {
   async getNewTitles() {
-    const animes = await http<Anime[]>(shikimori_api + 'animes' + newTitlesUrl);
+    const home_animes: HomeAnime[] = [];
 
-    return animes;
+    const animes = await http<AniemShort[]>(
+      shikimori_api + 'animes' + newTitlesUrl,
+    );
+
+    for (const el of animes) {
+      const genres = (
+        await http<AnimeFull>(shikimori_api + 'animes/' + el.id)
+      ).genres.map((item) => item.russian);
+
+      const screenshots = (
+        await http<Screenshot[]>(
+          shikimori_api + 'animes/' + el.id + '/screenshots',
+        )
+      ).map((item) => 'https://shikimori.one' + item.original.slice(0, -11));
+      screenshots.length = 10;
+
+      await new Promise((res) => setTimeout(res, 260));
+
+      home_animes.push({
+        id: el.id,
+        name: el.russian,
+        image: el.image.original.slice(0, -11),
+        score: el.score,
+        screenshots: screenshots,
+        genres: genres,
+      });
+    }
+
+    return home_animes;
   }
 }
