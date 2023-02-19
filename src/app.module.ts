fc3389449efc7182d5fd1e9da8d42be56da87209
Ivan <vanyablya003@gmail.com>
2023-02-19 17:48:34 +0300

    env add

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { HomeModule } from './Home/home.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
     HomeModule,
-    MongooseModule.forRoot('mongodb://127.0.0.1:27017/biskyDB'),
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI),
   ],
   controllers: [AppController],
   providers: [AppService],
