5c03ac840e9e3a211feb0911f89d9cde1634d2cf
Ivan <vanyablya003@gmail.com>
2023-03-05 21:46:40 +0300

    shiki cut

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { AuthModule } from './Auth/auth.module';
 import { HomeModule } from './Home/home.module';
-import { ShikiModule } from './Shiki/shiki.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SearchModule } from './Search/search.module';
@@ -13,7 +12,6 @@ import { SearchModule } from './Search/search.module';
   imports: [
     HomeModule,
     AuthModule,
-    ShikiModule,
     SearchModule,
     ConfigModule.forRoot({
       envFilePath: '.env',
